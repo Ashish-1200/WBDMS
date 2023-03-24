a12f4bf54983b4b4d6dfa939dcb75ee512e76426
flapperz <ashishkerion@gmail.com>
2023-03-24 17:59:10 -0400

    changes

--- web-based-document-system/frontend/document-app/src/app/incidentreports/incidentreports.component.ts
+++ web-based-document-system/frontend/document-app/src/app/incidentreports/incidentreports.component.ts
@@ -85,7 +85,7 @@ export class IncidentreportsComponent implements OnInit {
 
   }
 
-
+//change code
     delete(incident_id: any) {
       if (confirm('Are you sure you want to permanently delete this form?')) {
       this.incidentreportService.deleteUserrep(incident_id).subscribe((result) => {
