26f5edb7a2316c9404561d55483f02c3368a29f5
flapperz <ashishkerion@gmail.com>
2023-04-02 12:02:09 -0400

    changes

---
+++ web-based-document-system/frontend/document-app/src/app/intendedprojects/updateip/updateip.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from "@angular/core";
+import { HttpClient} from "@angular/common/http";
+
+
+@Injectable({ providedIn:"root"})
+  export class UpdateIPService {
+  constructor(private http: HttpClient ){ }
+
+  updateform(id:any,data:any){
+    return this.http.put("http://localhost:3000/incidentreport/" +id ,data)
+
+
+  }
+
+
+
+
+
+}
